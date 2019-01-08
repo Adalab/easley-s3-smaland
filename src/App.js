940972a7eaf3cd8ca3ef584fa8914201c952b6ef
aroatdlt <aroatato@gmx.com>
2019-01-08 20:38:06 +0100

    Add all files styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.scss';
+import './stylesheets/scss/main.scss';
 
 class App extends Component {
   render() {
