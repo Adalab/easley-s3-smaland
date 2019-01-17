756e2775608285aca80031763415fa1152bf96c0
lauraferrandof <lauraferrandof@gmail.com>
2019-01-17 20:01:44 +0100

    Create docs folder

---
+++ docs/precache-manifest.8346771922d2cee081b18c5d4db02b48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c06e538ffa8c6a9a1964f882518535c",
+    "url": "/static/media/silueta.1c06e538.jpg"
+  },
+  {
+    "revision": "aee038bb52fea8b3bcd57f2014777d95",
+    "url": "/static/media/restaurant.aee038bb.png"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "/static/media/descarga.0118e311.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c962de0dc1a0a88a552",
+    "url": "/static/js/main.3c962de0.chunk.js"
+  },
+  {
+    "revision": "578f23d6d3f71ca4dadb",
+    "url": "/static/js/1.578f23d6.chunk.js"
+  },
+  {
+    "revision": "3c962de0dc1a0a88a552",
+    "url": "/static/css/main.4b0a6869.chunk.css"
+  },
+  {
+    "revision": "ebd4d73cda375c928239276f0c6736cf",
+    "url": "/index.html"
+  }
+];
