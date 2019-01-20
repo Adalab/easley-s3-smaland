eea18a64e362cc061889fbeb7c7aef15f8b618b3
lauraferrandof <lauraferrandof@gmail.com>
2019-01-20 16:04:28 +0100

    Fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import LogoAdalab from '../images/logo-adalab.png';
+import LogoAdalab from "../images/logo-adalab.png";
 
 class Footer extends Component {
   render() {
@@ -7,9 +7,12 @@ class Footer extends Component {
       <footer className="footer__section">
         <p className="footer__text-reg">Awesome profile-cards &copy;2018</p>
         <a className="footer__conection-logo" href="https://adalab.es/">
-          <img className="footer__conection-logo-image" src={LogoAdalab} alt="Logo Adalab" />
+          <img
+            className="footer__conection-logo-image"
+            src={LogoAdalab}
+            alt="Logo Adalab"
+          />
         </a>
-        {/* <div className="footer__conection-logo"></div> */}
       </footer>
     );
   }
