00b4c8298a170709bbe99b1d23111eeadb0ecf9c
lauraferrandof <lauraferrandof@gmail.com>
2019-01-20 16:02:42 +0100

    Fix footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
+import LogoAdalab from '../images/logo-adalab.png';
 
 class Footer extends Component {
   render() {
     return (
-      <footer>
-        <section className="footer__section">
-          <p className="footer__text-reg">Awesome profile-cards &copy;2018</p>
-          <div className="footer__conection-logo"></div>
-        </section>
+      <footer className="footer__section">
+        <p className="footer__text-reg">Awesome profile-cards &copy;2018</p>
+        <a className="footer__conection-logo" href="https://adalab.es/">
+          <img className="footer__conection-logo-image" src={LogoAdalab} alt="Logo Adalab" />
+        </a>
+        {/* <div className="footer__conection-logo"></div> */}
       </footer>
     );
   }
