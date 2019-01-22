7e232414f0aaa34ced8b94182f4d3797362b911b
clara <clarisaharguindey@gmail.com>
2019-01-22 20:04:33 +0100

    create build version to publish

---
+++ docs/precache-manifest.4b3107c7c5cbee11ebd3190d4ff75893.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c06e538ffa8c6a9a1964f882518535c",
+    "url": "./static/media/silueta.1c06e538.jpg"
+  },
+  {
+    "revision": "aee038bb52fea8b3bcd57f2014777d95",
+    "url": "./static/media/restaurant.aee038bb.png"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/descarga.0118e311.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "088da103f2596645f141",
+    "url": "./static/js/main.088da103.chunk.js"
+  },
+  {
+    "revision": "583db3dd984afa6bb466",
+    "url": "./static/js/1.583db3dd.chunk.js"
+  },
+  {
+    "revision": "088da103f2596645f141",
+    "url": "./static/css/main.f7836f50.chunk.css"
+  },
+  {
+    "revision": "2f423bda18500ab182b7c07a29ecf2d0",
+    "url": "./index.html"
+  }
+];
