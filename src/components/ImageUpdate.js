43308b03134c0ac26dfca0345cfc028f2abf06d5
alteapin <alteap18@gmail.com>
2019-01-22 20:38:16 +0100

    adding small image

--- src/components/ImageUpdate.js
+++ src/components/ImageUpdate.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 
 class ImageUpdate extends Component {
     render() {
-        const {fakeFileClick, fileInput, dataBack} = this.props;
+        const { fakeFileClick, fileInput, dataBack } = this.props;
         return (
             <React.Fragment>
                 <p className="item-label">Imagen de perfil</p>
-                <form className="container-image">
-                    <button className="image-link" type="button" onClick={fakeFileClick}>Añadir imagen</button>
-                    <input type="file" ref={fileInput} name="" id="img-selector" /*className="action__hiddenField"*/ />
-                </form>
+                <div className="container-image">
+                    <button className="image-link" type="submit" onClick={fakeFileClick}>Añadir imagen</button>
+                    <input className="action__hiddenField" type="file" ref={fileInput} name="" id="img-selector"/>
                 <div className="image-preview image-preview-small">
-                    <img className="image-preview image-preview__file"src={dataBack.photo} alt={dataBack.name}></img>
+                    <img className="image-preview image-preview__file" src={dataBack.photo} alt={dataBack.name}></img>
                 </div>
-            </React.Fragment>
+                </div >
+            </React.Fragment >
         );
     }
 }
